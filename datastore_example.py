57a5329c94be2d71cdd098535973a232ee4a343f
amercader <amercadero@gmail.com>
2014-02-06 13:20:21 +0000

    Fix python script

--- datastore_example.py
+++ datastore_example.py
@@ -11,17 +11,14 @@ ckan = ckanapi.RemoteCKAN('http://127.0.0.1:5000', apikey=APIKEY)
 
 
 try:
-    package = ckan.action.package_show(id='test-datastore-packag2e')
-except ckanapi.errors.NotFound:
-    try:
-        print 'Creating package'
-        package = ckan.action.package_create(name='test-datastore-package')
-    except ckanapi.errors.ValidationError, e:
-        if e.error_dict.get('name') ==  ['That URL is already in use.']:
-            print 'Package already exists'
-            package = ckan.action.package_show(id='test-datastore-package')
-        else:
-            raise
+    print 'Creating package'
+    package = ckan.action.package_create(name='test-datastore-package')
+except ckanapi.errors.ValidationError, e:
+    if e.error_dict.get('name') ==  ['That URL is already in use.']:
+        print 'Package already exists'
+        package = ckan.action.package_show(id='test-datastore-package')
+    else:
+        raise
 
 while True:
 
